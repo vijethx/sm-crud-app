496b203d4069b9f7f6935aeb85771d56da8de3df
Vijeth (GitHub Desktop) <38877972+vijethx@users.noreply.github.com>
2022-10-01 00:38:49 +0530

    auth using firebase

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,30 @@
 import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 
-type Props = {};
+const Navbar = () => {
+  const [user] = useAuthState(auth);
 
-const Navbar = (props: Props) => {
+  const signUserOut = async () => {
+    await signOut(auth);
+  };
+
   return (
     <div className='navbar'>
-      <Link to='/'>Home</Link>
-      <Link to='/login'>Login</Link>
+      <div className='links'>
+        <Link to='/'>Home</Link>
+        {!user && <Link to='/login'>Login</Link>}
+      </div>
+      <div className='user'>
+        {user && (
+          <>
+            <p> {user?.displayName} </p>
+            <img src={user?.photoURL || ""} width='20' height='20' alt='pfp' />
+            <button onClick={signUserOut}> Log Out</button>
+          </>
+        )}
+      </div>
     </div>
   );
 };
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,38 @@
 import { auth, provider } from "../config/firebase";
 import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const [user] = useAuthState(auth);
+
+  const signUserOut = async () => {
+    await signOut(auth);
+  };
+
   const signInWithGoogle = async () => {
     const result = await signInWithPopup(auth, provider);
     console.log(result);
+    navigate("/");
   };
 
   return (
-    <div>
-      <p>Sign in with Google to continue</p>
-      <button onClick={signInWithGoogle}>Sign in with Google</button>
-    </div>
+    <>
+      {user ? (
+        <div>
+          <p>already signed in as {user?.displayName}</p>
+          <button onClick={signUserOut}>Log Out</button>
+        </div>
+      ) : (
+        <div>
+          <p>Sign in with Google to continue</p>
+          <button onClick={signInWithGoogle}>Sign in with Google</button>
+        </div>
+      )}
+    </>
   );
 };
 
