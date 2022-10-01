2de4e268ca058ae945dc9bc4b076e024dce8d211
Vijeth (GitHub Desktop) <38877972+vijethx@users.noreply.github.com>
2022-10-01 17:09:39 +0530

    like a post

--- src/pages/Main/Post.tsx
+++ src/pages/Main/Post.tsx
@@ -1,11 +1,54 @@
+import { addDoc, collection, query, where, getDocs } from "firebase/firestore";
 import { IPost } from "./index";
-
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../../config/firebase";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 interface Props {
   post: IPost;
 }
 
+interface Like {
+  userId: string;
+}
+
 export const Post = (props: Props) => {
+  const [likes, setLikes] = useState<Like[] | null>(null);
   const { post } = props;
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  const likesRef = collection(db, "likes");
+
+  const likesDoc = query(likesRef, where("postId", "==", post.id));
+
+  const getLikes = async () => {
+    const data = await getDocs(likesDoc);
+    setLikes(data.docs.map((doc) => ({ ...doc.data() })) as Like[]);
+  };
+
+  useEffect(() => {
+    getLikes();
+  }, []);
+
+  const addLike = async (data: any) => {
+    try {
+      await addDoc(likesRef, {
+        userId: user?.uid,
+        postId: post.id,
+      });
+      if (user) {
+        setLikes((prev) =>
+          prev ? [...prev, { userId: user?.uid }] : [{ userId: user?.uid }]
+        );
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const hasUserLiked = likes?.find((like) => like.userId === user?.uid);
+
   return (
     <div className='' key={post.id}>
       <div className='title'>
@@ -16,6 +59,10 @@ export const Post = (props: Props) => {
       </div>
       <div className='footer'>
         <p>@{post.username}</p>
+        <button onClick={addLike}>
+          {hasUserLiked ? <>&#128078;</> : <>&#128077;</>}
+        </button>
+        {likes && <p>Likes: {likes?.length}</p>}
       </div>
     </div>
   );
